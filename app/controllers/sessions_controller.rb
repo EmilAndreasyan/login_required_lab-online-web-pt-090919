45623b1620bedc0d5a9768c2daad025c75ba175e
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-02-27 04:43:15 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,6 +3,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+    return redirect_to(controller: 'sessions', action: 'new') if !session[:name] || session[:name].empty?
   end
 
   def destroy
